e9dd75497ce66da67d435c2d176d61a6561a1ed0
RhyanXG7 <143280548+RhyanXG7@users.noreply.github.com>
2024-10-24 16:00:59 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://github.com/notpoiu/mspaint/releases/latest/download/Script.luau"))()
+loadstring(game:HttpGet("https://github.com/RhyanXG7/Mspaint-keyless/releases/download/untagged-2fde54e19ddeb7fb9269/Script.luau"))()
